3991a5ded038a5b214949065bf9ee87d2265e09d
Kai Burgdorf <k.burgdorf@profihost.ag>
2019-12-16 09:37:10 +0100

    Solution for day 1

--- 01/main.pl
+++ 01/main.pl
@@ -4,5 +4,24 @@
 use strict;
 use warnings;
 
+use POSIX;
 
-print "test";
+
+my $path_puzzle = "./puzzle_input.txt";
+
+open (my $fh, '<:encoding(UTF-8)', $path_puzzle)
+	or die "Fehler beim öffnen von 'puzzle_input.txt'";
+
+
+my $fuel = 0;
+
+while (my $row = <$fh>) {
+	chomp $row;
+
+	
+
+	$fuel += POSIX::floor($row / 3) -2;
+
+}
+
+print ("Fuel required: " . $fuel . "\n");
